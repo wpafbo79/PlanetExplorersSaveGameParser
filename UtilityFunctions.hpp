da5a5ff436e4ff8027e4e5b75f90279c581928c6
Theron E. Weimer, Jr <wpafbo79@yahoo.com>
2018-08-14 22:53:41 -0400

    Added Doxygen comments to 'UtilityFunctions.hpp'.
    See #4.

--- UtilityFunctions.hpp
+++ UtilityFunctions.hpp
@@ -1,22 +1,45 @@
 #ifndef __UTILITYFUNCTIONS_HPP__
 #define __UTILITYFUNCTIONS_HPP__
+/**
+  @file utilityfunctions.hpp
+  @brief This is a library of utility functions.
+
+  A C-library style collection of utility functions.
+*/
 
 #include <string.h>
 
 /**
-  @author: Bruce Dawson (brucedawson)
-  @source: https://randomascii.wordpress.com/2013/04/03/stop-using-strncpy-already/
-NOTE: Commenters correctly pointed out that strncpy followed by null-termination
-      is not the ideal implementation of strcpy_safe because it is inefficient
-      (strncpy will zero all bytes to the end of the buffer) and maybe cut UTF-8
-      characters in half. Fixing this is beyond the scope of this post, which
-      was supposed to be focused on automatically inferring the buffer size
-      through template magic. So, don’t forget to implement YourCopyNFunction –
-      and maybe I’ll post a version next time.
+  @brief Safely copy a C-string ensuring a NULL termination.
+
+  From webpage:
+    "The syntax is a bit weird since it combines templating on an integral
+  value (instead of a type) with passing an array by reference, both of which
+  are unfamiliar to many programmers. For more information on passing arrays by
+  reference see this stack overflow [sic] article. Or, you can use the template
+  magic quite effectively without worrying about the details of how it works."
+
+  [Stack Overflow article](http://stackoverflow.com/questions/5724171/passing-an-array-by-reference)
+
+  @arg @c output A reference to the destination C-string of size @c charCount.
+  @arg @c pSrc The fixed size source C-string.
+  @author Bruce Dawson (brucedawson)
+  @remark Source:
+          https://randomascii.wordpress.com/2013/04/03/stop-using-strncpy-already/
+  @note From webpage:
+        "Commenters correctly pointed out that strncpy followed by
+        null-termination is not the ideal implementation of strcpy_safe because
+        it is inefficient (strncpy will zero all bytes to the end of the
+        buffer) and maybe cut UTF-8 characters in half. Fixing this is beyond
+        the scope of this post, which was supposed to be focused on
+        automatically inferring the buffer size through template magic. So,
+        don’t forget to implement YourCopyNFunction – and maybe I’ll post a
+        version next time."
 */
 template <size_t charCount>
 void strcpy_safe(char (& output)[charCount], const char * pSrc)
 {
+	/// @todo Implement YourCopyNFunction(output, pSrc, charCount);
 	//YourCopyNFunction(output, pSrc, charCount);
 	// Copy the string -- don't copy too many bytes.
 	strncpy(output, pSrc, charCount);
@@ -24,12 +47,24 @@ void strcpy_safe(char (& output)[charCount], const char * pSrc)
 	output[charCount - 1] = 0;
 }
 
+/**
+  @brief Safely copy a C-string ensuring a NULL termination.
+
+  Since @c strcpy_safe(char (& output)[charCount], const char * pSrc) does not
+  accept a dynamically allocated string, this version does.
+
+  @arg @c output A reference to the destination C-string of size @c charCount.
+  @arg @c pSrc The dynamically allocated source C-string.
+*/
 void strcpy_safe(char * (& output), const char * pSrc)
 {
 	size_t charCount = sizeof(output) / sizeof(output[0]);
 
+	// Don't accept a NULL C-string pointer.
+	/// @todo Thow an exception to indicate an error.
 	if(NULL == output) return;
 
+	// @todo Implement YourCopyNFunction(output, pSrc, charCount);
 	//YourCopyNFunction(output, pSrc, charCount);
 	// Copy the string -- don't copy too many bytes.
 	strncpy(output, pSrc, charCount);
